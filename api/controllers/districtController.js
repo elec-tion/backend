a7ee4d813e1614e1b59cd7970b384312d11fd111
baharsu <baharhuma6@gmail.com>
2024-04-14 00:07:53 +0300

    all api functions related to "District"

--- api/controllers/districtController.js
+++ api/controllers/districtController.js
@@ -3,10 +3,45 @@ const { exit } = require("process");
 const asyncHandler = require("express-async-handler");
 contractInstance.handleRevert = true;
 
-// @route GET /api/district
+// @route POST /api/district/:id/:name
+// @access private
+const addDistrict = asyncHandler(async (req, res) => {
+	// Create raw transaction
+	let rawTx = {
+		from: adminAccount.address,
+		to: contractInstance.options.address,
+		data: contractInstance.methods
+			.addDistrict(req.params.name, req.params.id)
+			.encodeABI(),
+	};
+
+	// estimate gas
+	const gasEstimate = await chain.eth.estimateGas(rawTx).catch((err) => {
+		console.error("Error estimating gas:", err);
+		exit(1); // Exit the process if there's an error
+	});
+
+	// convert gas estimate and set gas limit, gas price
+	const _gasLimit = chain.utils.numberToHex(gasEstimate);
+	rawTx.gasLimit = _gasLimit;
+	rawTx.gasPrice = "0x0";
+
+	// sign transaction
+	const signedTx = await adminAccount.signTransaction(rawTx);
+
+	// send transaction
+	const txr = await chain.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+	console.log("addDistrict", txr);
+	res.status(200).json({ success: 1 });
+});
+
+// @route GET /api/district/:id
 // @access private
 const getDistrict = asyncHandler(async (req, res) => {
-	const fCall = await contractInstance.methods.getDistrictDetails(req.params.id).call();
+	const fCall = await contractInstance.methods
+		.getDistrictDetails(req.params.id)
+		.call();
 
 	res.status(200).json({
 		id: fCall.districtID,
@@ -14,14 +49,82 @@ const getDistrict = asyncHandler(async (req, res) => {
 	});
 });
 
-// @route POST /api/district
+// @route PUT /api/district
 // @access private
-const addDistrict = asyncHandler(async (req, res) => {
+const addDistrictToElection = asyncHandler(async (req, res) => {
 	// Create raw transaction
 	let rawTx = {
 		from: adminAccount.address,
 		to: contractInstance.options.address,
-		data: contractInstance.methods.addDistrict(req.params.name, req.params.id).encodeABI(),
+		data: contractInstance.methods
+			.addDistrictToElection(req.params.electionId, req.params.districtId)
+			.encodeABI(),
+	};
+
+	// estimate gas
+	const gasEstimate = await chain.eth.estimateGas(rawTx).catch((err) => {
+		console.error("Error estimating gas:", err);
+		exit(1); // Exit the process if there's an error
+	});
+
+	// convert gas estimate and set gas limit, gas price
+	const _gasLimit = chain.utils.numberToHex(gasEstimate);
+	rawTx.gasLimit = _gasLimit;
+	rawTx.gasPrice = "0x0";
+
+	// sign transaction
+	const signedTx = await adminAccount.signTransaction(rawTx);
+
+	// send transaction
+	const txr = await chain.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+	console.log("addDistrictToElection", txr);
+	res.status(200).json({ success: 1 });
+});
+
+// @route DELETE /api/district/:id
+// @access private
+const removeDistrictFromElection = asyncHandler(async (req, res) => {
+	// Create raw transaction
+	let rawTx = {
+		from: adminAccount.address,
+		to: contractInstance.options.address,
+		data: contractInstance.methods
+			.removeDistrictFromElection(req.params.electionId, req.params.districtId)
+			.encodeABI(),
+	};
+
+	// estimate gas
+	const gasEstimate = await chain.eth.estimateGas(rawTx).catch((err) => {
+		console.error("Error estimating gas:", err);
+		exit(1); // Exit the process if there's an error
+	});
+
+	// convert gas estimate and set gas limit, gas price
+	const _gasLimit = chain.utils.numberToHex(gasEstimate);
+	rawTx.gasLimit = _gasLimit;
+	rawTx.gasPrice = "0x0";
+
+	// sign transaction
+	const signedTx = await adminAccount.signTransaction(rawTx);
+
+	// send transaction
+	const txr = await chain.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+	console.log("removeDistrictFromElection", txr);
+	res.status(200).json({ success: 1 });
+});
+
+// @route DELETE /api/district/:id
+// @access private
+const removeDistrict = asyncHandler(async (req, res) => {
+	// Create raw transaction
+	let rawTx = {
+		from: adminAccount.address,
+		to: contractInstance.options.address,
+		data: contractInstance.methods
+			.removeDistrict(req.params.id)
+			.encodeABI(),
 	};
 
 	// estimate gas
@@ -48,4 +151,7 @@ const addDistrict = asyncHandler(async (req, res) => {
 module.exports = {
 	getDistrict,
 	addDistrict,
+	removeDistrict,
+	addDistrictToElection,
+	removeDistrictFromElection,
 };
--- api/routes/districtRoutes.js
+++ api/routes/districtRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getDistrict, addDistrict } = require("../controllers/districtController");
+const { getDistrict, addDistrict, removeDistrict, addDistrictToElection, removeDistrictFromElection } = require("../controllers/districtController");
 
 router.route("/district/:id").get(getDistrict);
 router.route("/district/:id/:name").post(addDistrict);
+router.route("/district/:id").delete(removeDistrict);
+router.route("/district/:electionId/:districtId").post(addDistrictToElection);
+router.route("/district/:electionId/:districtId").delete(removeDistrictFromElection);
+
 module.exports = router;
