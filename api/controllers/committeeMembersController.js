723cbdd285f522e6adc404ca5610346082a69b97
Müslüm Barış Korkmazer <hello@babico.name.tr>
2024-04-14 19:11:08 +0300

    getElectionCommitteeMembersLength and getElectionCommitteeMemberDetails added to route and controller

--- api/controllers/committeeMembersController.js
+++ api/controllers/committeeMembersController.js
@@ -130,9 +130,31 @@ const removeElectionCommitteeMember = asyncHandler(async (req, res) => {
 	res.status(200).json({ success: 1 });
 });
 
+// @route GET /api/committeemember
+// @access private
+const getElectionCommitteeMemberLength = asyncHandler(async (req, res) => {
+	const fCall = await contractInstance.methods.getElectionCommitteeMemberLength().call();
+	res.status(200).json({
+		length: Number(fCall),
+	});
+});
+
+// @route GET /api/committeemember/:addr
+// @access private
+const getElectionCommitteeMemberDetails = asyncHandler(async (req, res) => {
+	const fCall = await contractInstance.methods.getElectionCommitteeMemberDetails(req.params.addr).call();
+	res.status(200).json({
+		id: Number(fCall.id),
+		name: fCall.name,
+		wallet: fCall.wallet,
+	});
+})
+
 module.exports = {
 	addElectionCommitteeMember,
 	addElectionCommitteeMemberToElection,
 	removeElectionCommitteeMemberFromElection,
 	removeElectionCommitteeMember,
+	getElectionCommitteeMemberLength,
+	getElectionCommitteeMemberDetails,
 };
--- api/routes/committeMembersRoutes.js
+++ api/routes/committeMembersRoutes.js
@@ -4,12 +4,15 @@ const {
     addElectionCommitteeMember,
     addElectionCommitteeMemberToElection,
     removeElectionCommitteeMemberFromElection,
-    removeElectionCommitteeMember
+    removeElectionCommitteeMember,
+    getElectionCommitteeMemberLength,
+    getElectionCommitteeMemberDetails,
 } = require("../controllers/committeeMembersController");
 
-router.route("/committeemember/:adddr/:name").post(addElectionCommitteeMember);
+router.route("/committeemember/:addr/:name").post(addElectionCommitteeMember);
 router.route("/committeememberwithelection/:addr/:elecId").post(addElectionCommitteeMemberToElection);
 router.route("/committeememberwithelection/:addr/:elecId").delete(removeElectionCommitteeMemberFromElection);
 router.route("/committeemember/:addr").delete(removeElectionCommitteeMember);
-
+router.route("/committeemember/:addr").get(getElectionCommitteeMemberDetails);
+router.route("/committeemember").get(getElectionCommitteeMemberLength);
 module.exports = router;
