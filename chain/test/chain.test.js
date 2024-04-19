ca1c61536c10b6b1037d82baebabdddd362f2e5f
Müslüm Barış Korkmazer <hello@babico.name.tr>
2024-04-19 19:41:18 +0300

    test of chain module

--- chain/test/chain.test.js
+++ chain/test/chain.test.js
@@ -1,30 +1,29 @@
-import chai from 'chai';
-const { expect } = chai;
-import fs from 'fs';
-import { chain, contractInstance, adminAccount } from '../index.js';
+const chai = require('chai');
+const fs = require('fs');
+const { chain, contractInstance, adminAccount } = require('../index.js');
 
 describe('Web3 Connection', function() {
     it('should connect to the blockchain', async function() {
         const accounts = await chain.eth.getAccounts();
-        expect(accounts).to.be.an('array').that.is.not.empty;
+        chai.expect(accounts).to.be.an('array').that.is.not.empty;
     });
 
     it('should get the current block number', async function() {
         const blockNumber = await chain.eth.getBlockNumber();
-        expect(blockNumber).to.be.a('bigint');
-        expect(parseInt(blockNumber)).to.be.above(0);
+        chai.expect(blockNumber).to.be.a('bigint');
+        chai.expect(parseInt(blockNumber)).to.be.above(0);
     });
 });
 
 describe('Contract Instance', function() {
     it('should be same Contract admin and the account used to deploy the contract', async function() {
         const contractAdmin = await contractInstance.methods.admin().call();
-        expect(contractAdmin).to.equal(adminAccount.address);
+        chai.expect(contractAdmin).to.equal(adminAccount.address);
     });
     it('should throw an error if ABI location is not defined', function() {
         delete process.env.CONTRACT_ABI_PATH;
 
-        expect(() => {
+        chai.expect(() => {
             contractInstance = new chain.eth.Contract(
                 JSON.parse(fs.readFileSync(process.env.CONTRACT_ABI_PATH, 'utf-8')),
                 fs.readFileSync(process.env.CONTRACT_ADDRESS_PATH, 'utf-8')
@@ -35,7 +34,7 @@ describe('Contract Instance', function() {
     it('should throw an error if contract address location is not defined', function() {
         delete process.env.CONTRACT_ADDRESS_PATH;
 
-        expect(() => {
+        chai.expect(() => {
             contractInstance = new chain.eth.Contract(
                 JSON.parse(fs.readFileSync(process.env.CONTRACT_ABI_PATH, 'utf-8')),
                 fs.readFileSync(process.env.CONTRACT_ADDRESS_PATH, 'utf-8')
