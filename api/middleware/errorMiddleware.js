a170b2b3a979ec29480e2472950776f2005da339
baharsu <baharhuma6@gmail.com>
2024-05-04 22:36:09 +0300

    added pino logger to log the error message same as the stack in json response body on hoppscotch

--- api/middleware/errorMiddleware.js
+++ api/middleware/errorMiddleware.js
@@ -1,6 +1,8 @@
+const logger = require("../utils");
+
 const errorHandler = (err, req, res, next) => {
 	const statusCode = res.statusCode ? res.statusCode : 500;
-
+	logger.error(err.message + '\n' + err.stack);
 	res.status(statusCode);
 	res.json({
 		message: err.message,
